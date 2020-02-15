b9b3cdb26a53e8fb112677dbd279adc2de80af34
ABHILASH <ss.abhilash3@gmail.com>
2020-02-15 13:58:07 +0530

    Add header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
+  const [todo, setTodo] = useState([
+    { text: 'one', key: '1' },
+    { text: 'two', key: '2' },
+    { text: 'three', key: '3' }
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header />
+      <View style={styles.content}>
+        {/* {todo form} */}
+        <View style={styles.list}>
+          <Text>sdfds</Text>
+          <FlatList data={todo} renderItem={({item}) => <Text>{item.text}</Text>} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +26,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
