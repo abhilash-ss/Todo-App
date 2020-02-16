2555643c58f49686094f566fa7a50d8ff186860e
ABHILASH <ss.abhilash3@gmail.com>
2020-02-17 00:25:45 +0530

    Add delete icon and update layout

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -6,13 +6,15 @@ import {
   FlatList,
   TouchableOpacity
 } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TodoItem({ item, pressHandler }) {
   return (
-    <TouchableOpacity>
-      <Text style={styles.item} onPress={() => pressHandler(item.key)}>
-        {item.text}
-      </Text>
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+      <View style={styles.item}>
+        <MaterialIcons name='delete' size={32} />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -24,6 +26,10 @@ const styles = StyleSheet.create({
     borderColor: '#bbb',
     borderWidth: 1,
     borderStyle: 'dashed',
-    borderRadius: 10
+    borderRadius: 10,
+    flexDirection: 'row'
+  },
+  itemText: {
+    marginLeft: 10
   }
 });
